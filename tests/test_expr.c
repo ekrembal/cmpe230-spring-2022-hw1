5b0e978519639f0aa258d18319c586b6c25d5bea
Oktay Özel <oozel@Oktays-MacBook-Air.local>
2022-04-08 23:10:24 +0300

    stack that empties the expression list given in postfix and test stack

--- tests/test_expr.c
+++ tests/test_expr.c
@@ -19,12 +19,10 @@ int main(){
     ParserGraph *graph = createParserGraph();
     tokenizeLine(graph, "choose(i,6,sqrt(x*y-5),choose(4,1, tr ( i ) , 0 )) + choose (4, 1, tr ( x*9 ) , 0 )");
 
-    struct variable temp1 = multiply(x, 9);
-    struct varibale temp2 = tr(temp1);
-    struct varibale temp3 = tr(temp1);
+  
     // tokenizeLine(graph, "choose(1, 2, 3, 4)");
     // tokenizeLine(graph, "7 + 8*9");
-    int len = tokenLen(tokens);
+    int len = tokenLen(tokens);    
     for(int i = 0; i < len; i++){
         printf("%s %s\n",tokenChars[i], enumToString(tokens[i]));
     }
---
+++ tests/test_stack_operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "../stack_operations.h"
+
+
+// i 6 x y * 5 - 4 1 i tr 0 Choose Choose 4 1 x 9 * tr 0 Choose +
+int main(){
+
+    printf("BASLADI\n");
+    struct List* temp =    createList();
+
+    // assert(temp != NULL);
+    assert(sizeOfList(temp) == 0);
+
+    struct Variable var;
+    var.name = "5";
+    var.feature = NUMBER;
+    var.variable_type = -1;
+    var.dim1=-1;
+    var.dim1=-1;
+
+    addToList(temp, var);
+
+    assert(sizeOfList(temp) == 1);
+
+
+    struct Variable var1;
+    var1.name = "+";
+    var1.feature = OPERATION;
+    var1.variable_type = 0;
+    var1.dim1=-1;
+    var1. dim1=-1;
+
+    addToList(temp, var1);
+
+    assert(sizeOfList(temp) == 2);
+
+    struct Variable var2;
+    var.name = "6";
+    var.feature = NUMBER;
+    var.variable_type = -1;
+    var.dim1=-1;
+    var.dim1=-1;
+
+    addToList(temp, var2);
+
+    assert(sizeOfList(temp) == 3);
+
+    printf("BITTI\n");
+    
+
+
+
+    return 0;
+}
