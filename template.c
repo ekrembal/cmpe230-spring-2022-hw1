3024380f7685f1b5c8066f32bca3c701ebd9c217
Oktay Özel <oozel@Oktays-MacBook-Air.local>
2022-04-09 12:56:11 +0300

    yepyeni template

--- template.c
+++ template.c
@@ -1,129 +1,140 @@
+#include <math.h>
 #define N 1000000
 #define M 1000
-struct variable{
-    int dim1, dim2, type;
-    int *a;
-};
 
+typedef struct Variable {
+    char *name;
+    int feature;
+    int dim1,dim2;
+    double val[1000][1000];
+}Variable;
 
-struct variable{
-    int dim1, dim2, type;
-}
-
-tr(variable(5, 1, vector)) -> variable(1, 5, matrix)
-
-testMultiply(variable(2, 4, matrix) , variable(4, 2, matrix)) > variable(2, 2, matrix, [1, 2, 3, 4])
-testMultiply(variable(2, 4, matrix) , variable(3, 2, matrix)) > NULL
-
-multiply(a1, a2);
-
-
-struct matrix{
-    int dim1;
-    int dim2;
-    int ar[M][M];
-};
-struct vector{
-    int dim;
-    int ar[N];
-};
-struct scalar{
-    int val;
-};
-// MATRIX X MATRIX
-struct variable multiply(struct variable, struct variable);
-void add(struct variable, struct variable);
-void subtract(struct variable, struct variable);
-struct matrix multiply(struct matrix mat1, struct matrix mat2){
-    struct matrix ans;
-    ans.dim1=mat1.dim1;
-    ans.dim2=mat2.dim2;
-    for( int i = 0 ; i < mat1.dim1 ; i++ ){
-        for( int j = 0 ; j < mat2.dim2 ; j++ ){
-            for( int k = 0 ; k < mat1.dim2 ){
-                ans.ar[i][j]+=(mat1.ar[i][k] * mat2.ar[k][j]);        
+struct Variable multiplication(struct Variable a, struct Variable b){
+    struct Variable* newNode = (struct Variable*)malloc(sizeof(struct Variable));
+    //SCALAR * SCALAR
+    if( (a.dim1 == 1 && a.dim2 == 1 ) && (b.dim1 == 1 && b.dim2 == 1)){
+        newNode->feature = SCA;
+        newNode.dim1=1;
+        newNode.dim2=1;
+        newNode.val[0][0] = a.val[0][0] * b.val[0][0];
+    }
+    //SCALAR * ( MATRIX OR VECTOR )
+    
+    if( (a.dim1 == 1 && a.dim2 == 1 ) && !(b.dim1 == 1 && b.dim2 == 1) ){
+        newNode->feature = MAT;
+        newNode.dim1=b.dim1;
+        newNode.dim2=b.dim2;
+        for( int i = 0 ; i < b.dim1 ; i++ ){
+            for( int j = 0 ; j < b.dim2 ; j++ ){
+                newNode.val[i][j] = a.val[0][0] * b.val[i][j];
             }
         }
     }
-    return ans;
-}
-// MATRIX X VECTOR
-struct vector multiply(struct matrix mat, struct vector vec){
-    struct vector ans;
-    ans.dim = mat.dim1;
-    for( int i = 0 ; i < mat.dim1 ; i++ ){
-        for( int j = 0 ; j < mat.dim2 ; j++ ){
-            ans.ar[i]+= ( mat.ar[i][j] * vec.ar[j]);
+    // (MATRIX OR VECTOR) * SCALAR
+    if( !(a.dim1 == 1 && a.dim2 == 1 ) && (b.dim1 == 1 && b.dim2 == 1) ){
+        newNode->feature = MAT;
+        newNode.dim1=a.dim1;
+        newNode.dim2=a.dim2;
+        for( int i = 0 ; i < a.dim1 ; i++ ){
+            for( int j = 0 ; j < a.dim2 ; j++ ){
+                newNode.val[i][j] = a.val[0][0] * a.val[i][j];
+            }
         }
     }
-    return ans;
-}
-// MATRIX X SCALAR
-struct matrix multiply(struct matrix mat, struct scalar c){
-    struct matrix ans;
-    ans.dim1 = mat.dim1;
-    ans.dim2 = mat.dim2;
-    for( int i = 0 ; i < mat.dim1 ; i++ ){
-        for( int j = 0 ; j <mat.dim2 ; j++){
-            ans.ar[i][j] = mat.ar[i][j] * c.val;
+    // (MATRIX OR VECTOR) * (MATRIX OR VECTOR)
+    if( !(a.dim1 == 1 && a.dim2 == 1 ) && !(b.dim1 == 1 && b.dim2 == 1) ){
+        newNode->feature = MAT;
+        newNode.dim1=a.dim1;
+        newNode.dim2=b.dim2;
+        for( int i = 0 ;  i < a.dim1 ; i++ ){
+            for( int j = 0 ; j < b.dim2 ; j++ ){
+                for( int k = 0 ; k < a.dim2 ; k++ ){
+                    newNode.val[i][j]+=(a.val[i][k] + b.val[k][j]);
+                }
+            }
         }
     }
-    return ans;
+    return newNode;
 }
-//VECTOR X VECTOR
-struct scalar multiply( struct vector vec1 , struct vector vec2){
-    struct scalar ans;
-    for( int i = 0 ; i < vec1.dim ; i++){
-        ans.val+=(vec1.ar[i] * vec2.ar[vec2.dim-i-1];       
+
+struct Variable generateScalarFromNumber(double var){
+    Variable *newNode = (Variable*)malloc(sizeof(Variable));
+    newNode->feature = SCA;
+    newNode->dim1 = 1;
+    newNode->dim2 = 1;
+    return newNode;
+}
+struct Variable addition( struct Variable a , struct Variable b ){
+    struct Variable* newNode = (struct Variable*)malloc(sizeof(struct Variable));
+    newNode->feature = a.feature;
+    newNode->dim1 = 1;
+    newNode->dim2 = 1;
+    for( int i = 0 ;  i < a.dim1 ; i++ ){
+        for(int j = 0 ; j < a.dim2 ; j++ ){
+            newNode.val[i][j] = a.val[i][j] + b.val[i][j];
+        }
     }
-    return ans;
+    return newNode;
 }
-//VECTOR X SCALAR
-struct vector multiply( struct vector vec , struct scalar c){
-    struct vector ans;
-    ans.dim = vec.dim;
-    for( int i = 0 ; i < ans.dim ; i++ ){
-        ans.ar[i]=vec.ar[i] * c.val;
+
+struct Variable substraction(struct Variable a, struct Variable b){
+    struct Variable* newNode = (struct Variable*)malloc(sizeof(struct Variable));
+    newNode->feature = a.feature;
+    newNode->dim1 = 1;
+    newNode->dim2 = 1;
+    for( int i = 0 ;  i < a.dim1 ; i++ ){
+        for(int j = 0 ; j < a.dim2 ; j++ ){
+            newNode.val[i][j] = a.val[i][j] - b.val[i][j];
+        }
     }
-    return ans;
+    return newNode;
 }
-// SCALAR X SCALAR
-struct scalar multiply(struct scalar a, struct scalar b){
-    struct scalar ans;
-    ans.val=a.val * b.val;
-    return ans.val;
+struct Variable sqrt( struct Variable a){
+    struct Variable* newNode = (struct Variable*)malloc(sizeof(struct Variable));
+    newNode->feature = a.feature;
+    newNode->dim1 = 1;
+    newNode->dim2 = 1;
+    newNode.val[0][0]=sqrt(a.val[0][0]);
+    return newNode;
 }
-// PRINT MATRIX
-void print_matrix(struct matrix mat){
-    for( int i = 0 ; i < mat.dim1 ; i++ ){
-        for( int j = 0 ; j < mat.dim2 ; j++ ){
-            printf("%d ",mar.ar[i][j]);
+struct Variable transpose( struct Variable a){
+    struct Variable* newNode = (struct Variable*)malloc(sizeof(struct Variable));
+    newNode->feature = a.feature;
+    newNode->dim1 = a.dim2;
+    newNode->dim2 = a.dim1;
+    for( int i = 0 ; i < a.dim1 ; i++ ){
+        for( int j = 0 ; j < a.dim2 ; j++ ){
+            newNode.val[j][i] = a.val[i][j];
+        }
+    }
+    return newNode;
+}
+struct Variable choose( struct Variable expr1, struct Variable expr2, struct Variable expr3, struct Variable expr4 ){
+    if( expr1.val[0][0] == 0 ){
+        return expr2;
+    }
+    else if( expr1.val[0][0] > 0){
+        return expr3;
+    }
+    else if( expr1.val[0][0] < 0 ){
+        return expr4;
+    }
+}
+void print( struct Variable a){
+    for( int i = 0 ; i < a.dim1 ; i++ ){
+        for( int  j = 0 ; j < a.dim2 ; j++ ){
+            printf("%lf",a.val[i][j]);
         }
         printf("\n");
     }
-    return
 }
-// PRINT VECTOR
-void print_vector(struct vector vec){
-    for( int i = 0 ; i < vec.dim ; i++ ){
-        printf("%d\n", vec.ar[i]);
-    }
-    return
-}
-//PRINT SCALAR
-void print_scalar(struct scalar c){
-    printf("%d\n",c.val );
-    return
-}
-void create_vector(){
 
-}
-void create_matrix(){
 
-}
-void create_scalar(){
 
-}
+
+
+
+
 /*
 # this program computes fibonacci
 # numbers
